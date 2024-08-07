ddb19dc3aad57b4d041219ebc33316ec8a15ffeb
mtalha44 <mt4458649@gmail.com>
2024-08-07 21:38:07 +0500

    Frontend

---
+++ Password hide and show/style.js
@@ -0,0 +1,19 @@
+var inputBox = document.querySelector(".password-input");
+function show()
+{
+        if(inputBox.type === 'password'){
+        input.type === 'text';
+  }  
+  else{
+    inputBox.type === 'password';
+  }
+}
+
+// function myFunction() {
+//     var x = document.getElementById("myInput");
+//     if (x.type === "password") {
+//       x.type = "text";
+//     } else {
+//       x.type = "password";
+//     }
+//   }
