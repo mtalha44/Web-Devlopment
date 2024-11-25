7e3c0753ff5b351e0ab54be81b096a8bb669a5e5
mtalha44 <mt4458649@gmail.com>
2024-11-25 23:01:01 +0500

    Learn scope of variable. ANd mention important topics in js

--- Js Revision/index.js
+++ Js Revision/index.js
@@ -94,19 +94,164 @@ function DoubleClick(){
 
 // Exception handling
 
-try{
-    setInterval(() => {
-        let today = new Date();
-        let time = today.toLocaleTimeString(today);
-        let timediv = document.querySelector(".trycatch");
+// try{
+//     setInterval(() => {
+//         let today = new Date();
+//         let time = today.toLocaleTimeString(today);
+//         let timediv = document.querySelector(".trycatch");
 
-        if(!timediv) throw new Error("Element with class .trycatch not found");
-        timediv.textContent = time;
-        console.log(timediv);
-    }, 1000);
+//         if(!timediv) throw new Error("Element with class .trycatch not found");
+//         timediv.textContent = time;
+//         console.log(timediv);
+//     }, 1000);
 
-}
-catch(error){
-    console.error("An error occured :" , error.message);
-}
+// }
+// catch(error){
+//     console.error("An error occured :" , error.message);
+// }
+
+
+// Callback
+// function Callback( callback ){
+
+//        console.log("data loading");
+//        return callback();
+// }
+
+// function display(){
+
+//     console.log("Data display")
+// }
+// Callback(display);
+
+// Callback Hell
+
+// function displayData( data , NextData){
+
+//     console.log("Data:",data);
+//     return NextData();
+
+// }
+
+// displayData( 5 , 
+//     function inputData(){
+//         displayData( 8  , 
+//             function inputData(){
+//                 displayData( 9 , 
+//                     function inputData(){
+//                         displayData( 10 )
+//                     }
+//                 )
+//             }
+//         )
+//     }
+//  )
+
+// Promise
+
+const promise = new Promise( ( resolve , reject  ) =>{
+
+      let url = 'https://jsonplaceholder.typicode.com/todos/1' ;
+      fetch(url)
+      .then((data) =>{
+
+          if(!data.ok) throw new Error( " Api not ok" );
+          return data.json();
+
+      })
+      .then( data => resolve(data))
+      .catch( err => reject(err))
+
+})
+  .then( data => console.log(data) )
+  .catch(err => console.error(err.message))
+
+//Scope
+
+// var globalVariable = "This is Global Variable";
+
+// function show(){ 
+//     console.log(globalVariable);   
+// }
+// show();
+
+//Learn Following Important Topics in Js :
+
+// Data types 
+// Premitive && non premetive 
+// Commments
+// Conditions 
+// Loops
+// Arrays
+// Objects
+// Functions
+// String Methods
+//     charAt 
+//     indexof
+//     split
+//     concat
+//     slice
+//     join
+//     replace
+//     toUpperCase
+//     toLowerCase
+//     substr
+//     substring
+//     search
+//     includes
+
+//  Array Methods
+//     push
+//     pop
+//     shift
+//     unshift
+//     splice
+//     sort
+//     reverse
+//     forEach
+//     map
+//     filter
+//     reduce
+//     find
+//     findIndex
+//     indexOf
+//     lastIndexOf 
+
+//     Browser Object Model
+//     Window
+//     Document
+//     Local Storage
+//     SetTimeout
+//     SetInterval
+//     ClearTimeout
+//     ClearInterval
+//     Date
+//     Math
+//     JSON
+//     Console
+//     Alert
+//     Prompt
+//     Confirm
+
+// Document Object Model
+//     HTML
+//     CSS
+//     JS
+//     DOM
+//     BOM
+//     Event
+//     Event Listener
+//     Event Object
+//     Event Handler
+
+// CallBack
+//     Callback Hell
+// Promise
+//     Promise Chaining
+// Async Await
+
+// Fetch Api
+//     Fetch Data
+//     Fetch Data with Promise
+//     Fetch Data with Async Await
 
